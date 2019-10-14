e8f53560c21e9b58863ad5858b5b214a8ffdfab2
onlinecoder <sum_nat1@yahoo.com>
2019-10-13 22:37:40 -0700

    Add files via upload

---
+++ msteams-samples-hello-world-nodejs-master/src/bot.js
@@ -0,0 +1,62 @@
+'use strict';
+
+module.exports.setup = function(app) {
+    var builder = require('botbuilder');
+    var teams = require('botbuilder-teams');
+    var config = require('config');
+
+    if (!config.has("bot.appId")) {
+        // We are running locally; fix up the location of the config directory and re-intialize config
+        process.env.NODE_CONFIG_DIR = "../config";
+        delete require.cache[require.resolve('config')];
+        config = require('config');
+    }
+    // Create a connector to handle the conversations
+    var connector = new teams.TeamsChatConnector({
+        // It is a bad idea to store secrets in config files. We try to read the settings from
+        // the config file (/config/default.json) OR then environment variables.
+        // See node config module (https://www.npmjs.com/package/config) on how to create config files for your Node.js environment.
+        appId: config.get("bot.appId"),
+        appPassword: config.get("bot.appPassword")
+    });
+    
+    var inMemoryBotStorage = new builder.MemoryBotStorage();
+    
+    var bot = new builder.UniversalBot(connector, function (session) {
+        console.info("SSSS bot woke up");
+        session.send("Hi... We sell shirts. Say 'show shirts' to see our products.");
+    }).set('storage', inMemoryBotStorage);
+    
+    // Add dialog to return list of shirts available
+    bot.dialog('getAlbums', function (session) {
+        console.info("SSSS showSHirts");
+        var msg = new builder.Message(session);
+        msg.attachmentLayout(builder.AttachmentLayout.carousel)
+        msg.attachments([
+            new builder.HeroCard(session)
+                .title("Classic White T-Shirt")
+                .subtitle("100% Soft and Luxurious Cotton")
+                .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+                .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/whiteshirt.png')])
+                .buttons([
+                    builder.CardAction.imBack(session, "buy classic white t-shirt", "Buy")
+                ]),
+            new builder.HeroCard(session)
+                .title("Classic Gray T-Shirt")
+                .subtitle("100% Soft and Luxurious Cotton")
+                .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+                .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/grayshirt.png')])
+                .buttons([
+                    builder.CardAction.imBack(session, "buy classic gray t-shirt", "Buy")
+                ])
+        ]);
+        session.send(msg).endDialog();
+    }).triggerAction({ matches: /^(getAlbums)/i });
+
+    // Setup an endpoint on the router for the bot to listen.
+    // NOTE: This endpoint cannot be changed and must be api/messages
+    app.post('/api/messages', connector.listen());
+
+    // Export the connector for any downstream integration - e.g. registering a messaging extension
+    module.exports.connector = connector;
+};
